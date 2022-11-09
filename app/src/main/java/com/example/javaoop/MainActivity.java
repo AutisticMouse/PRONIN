ec4bc80efe0646286db8dfa4c2e4e509f1bc21c6
student_ait405 <izmakovmorphium@mail.ru>
2022-11-09 09:37:37 +0300

    я не понял, но теперь больше котов

--- app/src/main/java/com/example/javaoop/MainActivity.java
+++ app/src/main/java/com/example/javaoop/MainActivity.java
@@ -1,42 +1,71 @@
 package com.example.javaoop;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
-
 import android.os.Bundle;
+import android.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private
+    //package access
+    //protected
+    //public
+
     Cat myCat;
+    int z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        z = z++;
+
         myCat = new Cat(4, "Puma");
-        myCat.talk();
+        myCat.talk(3);
 
 
-        Cat murzik = new Cat(2, "Murza");
+        Cat vaska = new Cat();
+        vaska.talk();
+        vaska.talk("Meow, meow!");
+        Log.i("field age", String.valueOf(vaska.age));
+
+
+        Cat murzik = new Lion(2, "Murza");
         murzik.talk();
         murzik.breathe();
 
 
         Cat glasha = new Cat();
         glasha.age = 1;
-        glasha.name = "Glasha";
+        glasha.name = "Masha";
         glasha.numberOfLegs = 4;
         Log.i("numberOfLegs", String.valueOf(glasha.numberOfLegs));
         glasha.talk();
 
         Puma puma = new Puma();
         puma.name = "Leo";
-        Log.i("breathe", "My name is " +
-                "" + puma.name + " and " + (puma.breathe()));
+
         puma.breathe();
         puma.talk();
         puma.isAlive = true;
-
         Log.i("isAlive", String.valueOf(puma.isAlive));
+        Log.i("breathe", "My name is " + puma.name + " and " + puma.breathe());
     }
+
+    void method(double x, int y) {
+        int a;
+        a = 1;
+        a = y + 1;
+        double d = x*2;
+        z = a + y;
+    }
+
+
+    void method1() {
+        int a;
+        a = 1;
+    }
+
+
 }
